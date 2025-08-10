11e41bcbd7a1fb149e213a974d01727f23591168
ghanou817 <abdelghanni78@gmail.com>
2025-08-10 12:32:44 +0100

    Change core API path to /core/ for frontend compatibility

--- backend/benzshop/urls.py
+++ backend/benzshop/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('core/', include('core.urls')),
 ]
 
 if settings.DEBUG:
