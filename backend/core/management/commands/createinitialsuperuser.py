7a338ffdf6755bb7850a53579cafee7e74c765fc
ghanou817 <abdelghanni78@gmail.com>
2025-08-11 00:27:06 +0100

    Render static/admin production fix

---
+++ backend/core/management/commands/createinitialsuperuser.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Creates initial superuser if not exists'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        username = 'abdelghani'
+        password = 'abdouabdou'
+        email = 'admin@example.com'
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" created.'))
+        else:
+            self.stdout.write(self.style.WARNING(f'Superuser "{username}" already exists.'))
