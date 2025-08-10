3da8264f8201bf166a25d8d854279e4634b1f704
ghanou817 <abdelghanni78@gmail.com>
2025-08-10 11:06:24 +0100

    enable CORS for Netlify frontend

--- backend/benzshop/settings.py
+++ backend/benzshop/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -88,6 +90,10 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
+CORS_ALLOWED_ORIGINS = [
+    "https://benzshop.netlify.app",
+]
 
 AUTH_PASSWORD_VALIDATORS = [
     {
